0d24b59d4346381dc7a3c8b5210a95059fbd340a
DarKRs <centralstreet_24@mail.ru>
2022-06-09 19:09:00 +0600

    Задача№1. Расчёт квадратного уравнения через дискриминант

---
+++ DevTask3.1/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTask3._1
+{
+    internal static class Calc
+    {
+        /// <summary>
+        /// Вычисление корней с помощью дискриминанта
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static (double?,double?) QuadEquation(double a,double b,double c)
+        {
+            double D = Math.Pow(b,2) - 4 * a * c;
+            return D >= 0 ? ((-b + Math.Sqrt(D)) / (2 * a), (-b - Math.Sqrt(D)) / (2 * a)) : (null,null);
+        }
+    }
+}
---
+++ DevTask3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevTask3._1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Calc.QuadEquation(1, 2, -3);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
