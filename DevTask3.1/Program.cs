3f16f4432b7fe1acfc7a34634d9cf40a575aae66
DarKRs <centralstreet_24@mail.ru>
2022-06-09 21:34:22 +0600

    Ввод с консоли

--- DevTask3.1/Program.cs
+++ DevTask3.1/Program.cs
@@ -6,9 +6,31 @@ namespace DevTask3._1
     {
         static void Main(string[] args)
         {
-            var a = Calc.QuadEquation(1, 2, -3);
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(a);
+            string?buf;
+            double[] koef;
+            double a, b, c;
+            Console.WriteLine("Данное консольное приложение решает квадратные уравнения. Введите 'exit' если хотите выйти\n");
+            do
+            {
+                Console.WriteLine("Введите коэфициенты через пробел - прим. (1 2 -3):");
+                buf = Console.ReadLine();
+                try
+                {
+                    koef = buf.Split(' ')
+                        .Where(x => !string.IsNullOrEmpty(x))
+                        .Select(x => Convert.ToDouble(x)).ToArray();
+                }
+                catch(Exception ex)
+                {
+                    if(buf != "exit")
+                        Console.WriteLine("\nКоэфициенты введены неправильно");
+                    continue;
+                }
+                Console.WriteLine("\nРезультаты X1, X2:");
+                Console.WriteLine(Calc.QuadEquation(koef[0], koef[1], koef[2]));
+            } while(buf != "exit");
+
+            Console.WriteLine("\nВыполнение приложения прервано");
             Console.ReadKey();
         }
     }
